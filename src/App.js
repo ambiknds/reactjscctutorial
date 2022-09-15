0c74bdf8c29fba44fb901c791817d85976451f01
ambiknds <ambikndsjobz@gmail.com>
2022-09-15 08:22:15 +0530

    add employee block

--- src/App.js
+++ src/App.js
@@ -15,9 +15,56 @@ function App() {
             setRole(e.target.value);
           }}
         />
-        <Employee name="Ambi" role="Intern"/>
-        <Employee name="Abby" role={role}/>
-        <Employee name="John"/>
+        <div className='flex flex-wrap justify-center'>
+          <Employee 
+            name="Ambi" 
+            role="Intern" 
+            img="https://images.pexels.com/photos/1450114/pexels-photo-1450114.jpeg?auto=compress&cs=tinysrgb&w=400"
+
+          />
+          <Employee 
+            name="Abby" 
+            role={role} 
+            img="https://images.pexels.com/photos/3771807/pexels-photo-3771807.jpeg?auto=compress&cs=tinysrgb&w=400"
+          />
+          <Employee 
+            name="John"
+            role={role}
+            img="https://images.pexels.com/photos/3771839/pexels-photo-3771839.jpeg?auto=compress&cs=tinysrgb&w=400"  
+          />
+          <Employee 
+            name="Ambi" 
+            role="Intern" 
+            img="https://images.pexels.com/photos/1450114/pexels-photo-1450114.jpeg?auto=compress&cs=tinysrgb&w=400"
+
+          />
+          <Employee 
+            name="Abby" 
+            role={role} 
+            img="https://images.pexels.com/photos/3771807/pexels-photo-3771807.jpeg?auto=compress&cs=tinysrgb&w=400"
+          />
+          <Employee 
+            name="John"
+            role={role}
+            img="https://images.pexels.com/photos/3771839/pexels-photo-3771839.jpeg?auto=compress&cs=tinysrgb&w=400"  
+          />
+          <Employee 
+            name="Ambi" 
+            role="Intern" 
+            img="https://images.pexels.com/photos/1450114/pexels-photo-1450114.jpeg?auto=compress&cs=tinysrgb&w=400"
+
+          />
+          <Employee 
+            name="Abby" 
+            role={role} 
+            img="https://images.pexels.com/photos/3771807/pexels-photo-3771807.jpeg?auto=compress&cs=tinysrgb&w=400"
+          />
+          <Employee 
+            name="John"
+            role={role}
+            img="https://images.pexels.com/photos/3771839/pexels-photo-3771839.jpeg?auto=compress&cs=tinysrgb&w=400"  
+          />
+        </div>
       </>
       ) : (
        <p>You cannot see the employempmes</p>
