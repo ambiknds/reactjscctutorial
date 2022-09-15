55471cde1b02e826772caf2103569c5e8ccdbf5d
ambiknds <ambikndsjobz@gmail.com>
2022-09-15 07:43:14 +0530

    change2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import './App.css';
 import Employee from './components/Employee';
-
+import {useState} from 'react';
 
 function App() {
+  const [role, setRole] = useState('dev');
   const showEmployees = true;
+  
   return (
     <div className="App">
-    {showEmployees ? 
+    {showEmployees ? (
       <>  
+        <input type = "text"
+          onChange={(e) => {
+            setRole(e.target.value);
+          }}
+        />
         <Employee name="Ambi" role="Intern"/>
-        <Employee name="Abby
-        "/>
+        <Employee name="Abby" role={role}/>
         <Employee name="John"/>
       </>
-       :
+      ) : (
        <p>You cannot see the employempmes</p>
-    }
+       )}
     </div>
   );
 }
