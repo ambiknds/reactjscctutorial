a2aebf1f9cb25c84a1c6e869ff7dd97fc8edc87f
ambiknds <ambikndsjobz@gmail.com>
2022-09-15 09:06:54 +0530

    edit and modify employee, uuid installation, array map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import './index.css';
 import Employee from './components/Employee';
 import {useState} from 'react';
+import {v4 as uuidv4} from 'uuid';
 
 function App() {
   const [role, setRole] = useState('dev');
+  const [employees, setEmployees] = useState(
+    [
+      {
+        name:"Ambi", 
+        role:"Dev", 
+        img: "https://images.pexels.com/photos/1450114/pexels-photo-1450114.jpeg"
+      },
+      {
+        name:"John", 
+        role:"None", 
+        img:"https://images.pexels.com/photos/3771807/pexels-photo-3771807.jpeg"
+      },
+      {
+        name:"Jack",
+        role:"None",
+        img:"https://images.pexels.com/photos/5648100/pexels-photo-5648100.jpeg"
+      },
+      {
+        name:"Alan",
+        role:"None",
+        img:"https://images.pexels.com/photos/3894495/pexels-photo-3894495.jpeg"
+      },{
+        name:"Tom",
+        role:"None",
+        img:"https://images.pexels.com/photos/4041089/pexels-photo-4041089.jpeg"
+      },{
+        name:"Alice",
+        role:"None",
+        img:"https://images.pexels.com/photos/13328567/pexels-photo-13328567.jpeg"
+      }
+    ]
+  )
   const showEmployees = true;
-  
   return (
     <div className="App bg-slate-200">
     {showEmployees ? (
@@ -16,54 +48,17 @@ function App() {
           }}
         />
         <div className='flex flex-wrap justify-center'>
-          <Employee 
-            name="Ambi" 
-            role="Intern" 
-            img="https://images.pexels.com/photos/1450114/pexels-photo-1450114.jpeg?auto=compress&cs=tinysrgb&w=400"
-
-          />
-          <Employee 
-            name="Abby" 
-            role={role} 
-            img="https://images.pexels.com/photos/3771807/pexels-photo-3771807.jpeg?auto=compress&cs=tinysrgb&w=400"
-          />
-          <Employee 
-            name="John"
-            role={role}
-            img="https://images.pexels.com/photos/3771839/pexels-photo-3771839.jpeg?auto=compress&cs=tinysrgb&w=400"  
-          />
-          <Employee 
-            name="Ambi" 
-            role="Intern" 
-            img="https://images.pexels.com/photos/1450114/pexels-photo-1450114.jpeg?auto=compress&cs=tinysrgb&w=400"
-
-          />
-          <Employee 
-            name="Abby" 
-            role={role} 
-            img="https://images.pexels.com/photos/3771807/pexels-photo-3771807.jpeg?auto=compress&cs=tinysrgb&w=400"
-          />
-          <Employee 
-            name="John"
-            role={role}
-            img="https://images.pexels.com/photos/3771839/pexels-photo-3771839.jpeg?auto=compress&cs=tinysrgb&w=400"  
-          />
-          <Employee 
-            name="Ambi" 
-            role="Intern" 
-            img="https://images.pexels.com/photos/1450114/pexels-photo-1450114.jpeg?auto=compress&cs=tinysrgb&w=400"
-
-          />
-          <Employee 
-            name="Abby" 
-            role={role} 
-            img="https://images.pexels.com/photos/3771807/pexels-photo-3771807.jpeg?auto=compress&cs=tinysrgb&w=400"
-          />
-          <Employee 
-            name="John"
-            role={role}
-            img="https://images.pexels.com/photos/3771839/pexels-photo-3771839.jpeg?auto=compress&cs=tinysrgb&w=400"  
-          />
+          {employees.map((employee) => {
+              {/* console.log(uuidv4()) */}
+            return (
+              <Employee 
+              key={uuidv4()}
+              name={employee.name}
+              role={employee.role}
+              img ={employee.img}
+            />
+            )
+          })}
         </div>
       </>
       ) : (
