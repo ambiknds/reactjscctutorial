070cac44b5b55a4808e7521c4c0943d2e091a33d
ambition <ambikndsjobz@gmail.com>
2022-11-04 07:21:26 +0530

    add fetch customers from backend

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
 
 function Customers() {
+  const [customers, setCustomers] = useState();
+  useEffect(() => {
+    console.log('Fetching...')
+    fetch('http://localhost:8000/api/customers/')
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      setCustomers(data.customers);
+    })
+  }, [])
   return (
-    <div>Customers</div>
+    <div>
+      <h1>Here are our Customers</h1>
+      
+      {customers ?
+        customers.map((customer) => {
+        return (
+          <div>
+            <table >
+              <tr>
+                <td className='border-black border-2'>{customer.name}</td>
+                <td className='border-black border-2'>{customer.industry}</td>
+              </tr>
+            </table>
+          </div>
+        )
+      })
+      : <p>Unable to load customer</p>
+      }
+    </div>
   )
 }
 
